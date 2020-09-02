f44218fb088ac6e6411c8be4aa556f82b86dbe8c
cirine <1410@holbertonschool.com>
2020-09-03 00:36:09 +0100

    try

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -35,10 +35,10 @@ def find_int(n):
     return str(n) + ' is a number'
 
 
-@app.route('/number_template/<int:n>')
-def templateifint(n=n):
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def templateifint(n):
     '''display template if n is int'''
-    return render_template('5-number.html', name=name)
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
